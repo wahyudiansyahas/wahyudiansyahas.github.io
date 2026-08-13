778aac8a730c54c807494efbe9cc3d4d9a551458
wahyudiansyahas <wahyudiansyahas@gmail.com>
2026-08-13 13:41:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ const portfolioDocumentDataset = [
   { title: "Job Description", category: "recruitment", extension: "pdf", path: "Portofolio/jd.pdf" },
   { title: "Job Vacancy", category: "recruitment", extension: "png", path: "Portofolio/jobvacancy.png" },
   { title: "Job Posting", category: "recruitment", extension: "png", path: "Portofolio/zoho.png" },
-  { title: "Applicant Tracking System", category: "recruitment", extension: "png", path: "Portofolio/ats.png" },
+  { title: "Applicant Tracking System", category: "recruitment", extension: "png", path: "Portofolio/ats.pdf" },
   { title: "Offering Letter", category: "recruitment", extension: "pdf", path: "Portofolio/ol.pdf" },
   { title: "Interview Evaluation", category: "recruitment", extension: "pdf", path: "Portofolio/formwawancara.pdf" },
   { title: "Recruitment Dashboard", category: "recruitment", extension: "xlsx", path: "Portofolio/recruitment.xlsx" },
